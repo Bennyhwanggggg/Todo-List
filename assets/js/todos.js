ff6bbd7f527bc5d46a1aa9154607c8435f5a5931
Benny Hwang <32606136+Bennyhwanggggg@users.noreply.github.com>
2018-02-23 13:39:10 +1100

    css improved and save and mark all as complete function to be added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -16,7 +16,11 @@ $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var newTodo = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span> " + newTodo + "</li>");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + newTodo + "</li>");
 	}
 });
 
+// Click pencil to edit function
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
